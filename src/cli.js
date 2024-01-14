0e7fa01e1bbaf0e24b5b4a662a3adde65d1b750c
sarvanshiriev <sarvan.shiriev@mail.ru>
2024-01-14 16:32:04 +0500

    Реализован пункт 3

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+// src/cli.js
+import readlineSync from 'readline-sync';
+
+export default function greetUser() {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+}
